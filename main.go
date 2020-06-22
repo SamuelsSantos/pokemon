31bb47bfa39870ae57d29297f82577b8d20bc0d0
samuelssantos <samuelsantos.sjdr@gmail.com>
2020-06-22 00:29:33 -0300

    Remover charactere quebra de linha

--- main.go
+++ main.go
@@ -19,14 +19,12 @@ func processar(movements string) {
 	} else {
 		fmt.Println("Jornada inválida!")
 	}
-
 }
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Movimentos válidos: N, S, E ou O sendo (Norte, Sul, Este, Oeste)")
 	fmt.Print("Digite a jornada do Ash: ")
 	movimentos, _ := reader.ReadString('\n')
-	processar(movimentos)
+	processar(movimentos[:len(movimentos)-1])
 }
